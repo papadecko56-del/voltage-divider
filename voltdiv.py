005e4815cca46586ea42a000558d9474e552b6e9
papadecko56-del <papadecko56@gmail.com>
2026-05-17 12:53:59 +0000

    Improve error handling and input validation
    Refactor voltage divider code to handle zero resistance and input errors.

--- voltdiv.py
+++ voltdiv.py
@@ -2,32 +2,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # simple voltage divider
-R1 = float(input("resistance in ohms: "))
-R2 = float(input("resistance in ohms: "))
-Vs = float(input("source voltage in volts: "))
-
 
 
 try:
-   R2 / (R1 + R2)
- 
-   if R1 < 0 or R2 < 0:
+     R1 = float(input("resistance in ohms: "))
+     R2 = float(input("resistance in ohms: "))
+     Vs = float(input("source voltage in volts: "))
+    
+
+     
+     if R1 <= 0 or R2 <= 0:
        
    
-        print("Error  negative resistance choose positive") 
+          print("Error  negative or zero resistance choose positive") 
+       
+     else:
+          V_out = Vs * (R2 / (R1 + R2))
+          print(f"Output voltage:{V_out} V")
+
+          labels = ['R1 drop', 'R2 drop(Vout)']
+          values = [Vs - V_out, V_out]
+          plt.bar(labels, values, color=['#3b82f6', '#00e5ff'])
+          plt.title('Voltage Divider')
+          plt.xlabel('Values')
+          plt.ylabel('Voltage (V)')
+          plt.show()
+
      
 
+except ValueError:
+ print("use numbers as inputs")
 
-   else:
-        V_out = Vs * (R2 / (R1 + R2))
-        print(f"Output voltage:{V_out} V")
-# create a graph that shows the relationship between voltage sources
-        labels = ['R1 drop', 'R2 drop(Vout)']
-        values = [Vs - V_out, V_out]
-        plt.bar(labels, values, color=['#3b82f6', '#00e5ff'])
-        plt.title('Voltage Divider')
-        plt.xlabel('Values')
-        plt.ylabel('Voltage (V)')
-        plt.show()
-except ZeroDivisionError: 
-    print("zero division use positives")
+
